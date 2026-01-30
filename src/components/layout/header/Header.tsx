85e0886bc55e88b1ae4ee574f6b0ef8a95b3109b
Ong Sen Hong <seghot@gmail.com>
2026-01-30 11:05:11 +0800

    feat: enhance Header component with navigation handling and type definitions

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,11 +1,25 @@
+import { useLocation, useNavigate } from 'react-router';
 import AnimeSearchBox from './AnimeSearchBox';
 
+interface NavItem {
+  name: string;
+  href: string;
+}
+
 const Header = () => {
-  const navItems = [
+  const navItems: NavItem[] = [
     { name: 'Home', href: '/' },
     { name: 'Catalog', href: '/anime' },
     { name: 'Favorites', href: '/favorites' },
   ];
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavigate = (e: React.MouseEvent, href: string) => {
+    e.preventDefault(); // prevent href"#"
+    navigate(href);
+  };
 
   return (
     <header className="fixed top-0 z-50 w-full h-20 backdrop-blur-md border-b  text-white border-white/10 flex items-center px-10 ">
@@ -18,16 +32,25 @@ const Header = () => {
             AnimeSearch
           </a>
 
-          <ul className="flex items-center gap-4">
-            {navItems.map((item) => (
-              <li key={item.name}>
-                <a
-                  href={item.href}
-                  className="px-3 py-2 text-gray-300 hover:text-gray-50 rounded-md transition-all">
-                  {item.name}
-                </a>
-              </li>
-            ))}
+          <ul className="flex items-center gap-6">
+            {' '}
+            {navItems.map((item) => {
+              const isActive = location.pathname === item.href;
+
+              return (
+                <li key={item.name}>
+                  <a
+                    onClick={(e) => handleNavigate(e, item.href)}
+                    href={item.href} // for "right click open in new tab, block default behavior in handleNavigate"
+                    className={`
+                           px-3 py-2 text-md font-medium transition-all duration-200
+                         text-gray-400 hover:text-white 
+                        `}>
+                    {item.name}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
 
