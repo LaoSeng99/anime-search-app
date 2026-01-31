17a4863d078ced2c7ce9bf22c1c5eeae569c7da8
Ong Sen Hong <seghot@gmail.com>
2026-01-31 21:49:23 +0800

    feat: implement Dialog component and context; refactor App to use ComposeProviders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from 'react-router';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './services/queryClient';
 import { router } from './routes';
 import { useEffect } from 'react';
 import { useFavouriteStore } from './hooks/useFavouriteStore';
+import { ComposeProviders } from './context/ComposeProviders';
+import { DialogProvider } from './context/DialogContext';
+import { ReactQueryProvider } from './context/ReactQueryProvider';
+
+const providers = [ReactQueryProvider, DialogProvider];
 
 function App() {
   useEffect(() => {
@@ -11,9 +14,9 @@ function App() {
   }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <ComposeProviders providers={providers}>
       <RouterProvider router={router} />
-    </QueryClientProvider>
+    </ComposeProviders>
   );
 }
 
