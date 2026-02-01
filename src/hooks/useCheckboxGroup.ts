4435e56a8563d14da21a68ac9c0900e99df570e7
Ong Sen Hong <seghot@gmail.com>
2026-02-02 00:08:13 +0800

    refactor: update useCheckboxGroup more focus on status update

--- src/hooks/useCheckboxGroup.ts
+++ src/hooks/useCheckboxGroup.ts
@@ -1,24 +1,80 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import type { CheckboxOption } from '../types/ui.interface';
 
-export const useCheckboxGroup = (initialOptions: CheckboxOption[]) => {
-  const [options, setOptions] = useState<CheckboxOption[]>(initialOptions);
+/**
+ * Not support single
+ */
+export const useCheckboxGroup = (
+  initialOptions: CheckboxOption[],
+  /**
+   * return currently selected Ids
+   */
+  onChange?: (selectedIds: string[]) => void,
+) => {
+  const [selectedIdSet, setSelectedIdSet] = useState<Set<string>>(
+    () => new Set(initialOptions.filter((o) => o.checked).map((o) => o.id)),
+  );
 
-  useEffect(() => {
-    setOptions(initialOptions);
-  }, [initialOptions]);
+  const handleUpdate = useCallback(
+    (updater: (prev: Set<string>) => Set<string>) => {
+      setSelectedIdSet((prev) => {
+        const next = updater(new Set(prev));
+        onChange?.(Array.from(next));
+        return next;
+      });
+    },
+    [onChange],
+  );
 
-  const toggleOption = (id: string) => {
-    setOptions((prev) =>
-      prev.map((opt) =>
-        opt.id === id ? { ...opt, checked: !opt.checked } : opt,
-      ),
+  // multi Toggle
+  const toggleOption = useCallback(
+    (id: string) => {
+      handleUpdate((set) => {
+        if (set.has(id)) set.delete(id);
+        else set.add(id);
+        return set;
+      });
+    },
+    [handleUpdate],
+  );
+
+  const toggleSingle = useCallback(
+    (id: string) => {
+      handleUpdate((set) => {
+        if (set.has(id)) {
+          set.delete(id);
+        } else {
+          set.clear();
+          set.add(id);
+        }
+        return set;
+      });
+    },
+    [handleUpdate],
+  );
+
+  // 重置
+  const reset = useCallback(() => {
+    handleUpdate(
+      () => new Set(initialOptions.filter((o) => o.checked).map((o) => o.id)),
     );
+  }, [handleUpdate, initialOptions]);
+
+  const options = useMemo(() => {
+    return initialOptions.map((opt) => ({
+      ...opt,
+      checked: selectedIdSet.has(opt.id),
+    }));
+  }, [initialOptions, selectedIdSet]);
+
+  const selectedIds = useMemo(() => Array.from(selectedIdSet), [selectedIdSet]);
+
+  return {
+    options,
+    toggleOption,
+    toggleSingle,
+    reset,
+    selectedIds,
+    setSelectedIds: (ids: string[]) => setSelectedIdSet(new Set(ids)),
   };
-
-  const reset = () => setOptions(initialOptions);
-
-  const selectedIds = options.filter((o) => o.checked).map((o) => o.id);
-
-  return { options, toggleOption, reset, selectedIds };
 };
