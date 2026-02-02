9199fdcf8f6e7d9ed184b78cef744d1246a4f382
Ong Sen Hong <seghot@gmail.com>
2026-02-02 22:16:13 +0800

    hotfix: production issue (react-video),  remove manual chunk file

--- vite.config.ts
+++ vite.config.ts
@@ -4,41 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          const path = id.replace(/\\/g, '/');
-
-          if (path.includes('node_modules')) {
-            if (
-              path.includes('hls.js') ||
-              path.includes('dashjs') ||
-              path.includes('react-player') ||
-              path.includes('@mux')
-            ) {
-              return 'video-player-engine';
-            }
-
-            if (
-              path.includes('react') ||
-              path.includes('@tanstack') ||
-              path.includes('react-router')
-            ) {
-              return 'framework-core';
-            }
-
-            if (path.includes('framer-motion')) {
-              return 'ui-animation';
-            }
-
-            return 'vendor-stable';
-          }
-        },
-      },
-    },
-  },
   plugins: [
     react(),
     tailwindcss(),
