87e5c360947d281c7790b09533c55b91c56cba5c
Ong Sen Hong <seghot@gmail.com>
2026-02-02 17:30:00 +0800

    fix: add new tailwind breakpoint; update build bundle chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,47 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-
+import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          const path = id.replace(/\\/g, '/');
+
+          if (path.includes('node_modules')) {
+            if (
+              path.includes('hls.js') ||
+              path.includes('dashjs') ||
+              path.includes('react-player') ||
+              path.includes('@mux')
+            ) {
+              return 'video-player-engine';
+            }
+
+            if (
+              path.includes('react') ||
+              path.includes('@tanstack') ||
+              path.includes('react-router')
+            ) {
+              return 'framework-core';
+            }
+
+            if (path.includes('framer-motion')) {
+              return 'ui-animation';
+            }
+
+            return 'vendor-stable';
+          }
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({ open: true, filename: 'bundle-analysis.html' }),
+  ],
 });
