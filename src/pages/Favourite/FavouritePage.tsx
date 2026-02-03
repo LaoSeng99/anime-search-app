3b0204e525ada35ae42f7daf5aa91b2950f48840
Ong Sen Hong <seghot@gmail.com>
2026-02-03 23:19:06 +0800

    enhance: Favourite page mobile responsive; Home page content section position

--- src/pages/Favourite/FavouritePage.tsx
+++ src/pages/Favourite/FavouritePage.tsx
@@ -1,5 +1,5 @@
 import PaginationGroup from '../../components/ui/PaginationGroup';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import AnimePosterCard from '../../components/AnimePosterCard';
 import { useDialog } from '../../hooks/useDialog';
 import BackgroundSection from '../../components/layout/BackgroundSection';
@@ -8,6 +8,7 @@ import { useFavouriteLogic } from './FavouritePage.hook';
 import FavouriteToolbar from './FavouriteToolbar';
 import { Heart } from 'lucide-react';
 import EmptyState from '../../components/ui/EmptyState';
+import { cn } from '../../utils/ui.util';
 
 const FavouritePage = () => {
   const {
@@ -21,7 +22,6 @@ const FavouritePage = () => {
     handleSorter,
     resetFilter,
     removeAllFavourite,
-    hasFavourite,
   } = useFavouriteLogic();
 
   const { confirm } = useDialog();
@@ -29,8 +29,6 @@ const FavouritePage = () => {
   const currentBackgroundUrl = useMemo(() => {
     return displayAnime[0]?.images?.webp?.image_url ?? '';
   }, [displayAnime]);
-
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleRemoveAllFavourite = () => {
     confirm(
@@ -52,15 +50,26 @@ const FavouritePage = () => {
 
       <div className="absolute inset-0 z-10 backdrop-blur-sm bg-black/40" />
       {/* Content */}
-      <section className="relative z-20 h-full  min-w-full flex flex-col items-center pt-48">
-        <h1 className="text-white text-4xl md:text-6xl font-extrabold tracking-tight drop-shadow-xl self-start px-8 md:px-16 mb-8">
-          Favourite Anime
+      <section
+        className={cn([
+          'relative z-20 h-full min-w-full flex flex-col items-center',
+          'pt-18',
+        ])}>
+        <h1
+          className={cn([
+            'text-white  font-extrabold tracking-tight drop-shadow-xl self-start   w-full h-full',
+            'text-3xl sm:text-4xl md:text-5xl lg:text-6xl',
+            'px-8 md:px-16',
+            'py-8 pb-0 lg:pb-8',
+            ' bg-black/80 lg:bg-transparent',
+          ])}>
+          Anime Catalog
         </h1>
 
-        <div className="w-full  bg-black/80 px-8 py-12 min-h-150">
+        <div className="w-full  bg-black/80 px-8 py-8 min-h-150 flex flex-col justify-between">
           {/* Action Group */}
           <FavouriteToolbar
-            isLoading={isLoading}
+            isLoading={false}
             totalItems={totalItems}
             filterOptions={filterOptions}
             onFilter={handleFilter}
@@ -71,9 +80,11 @@ const FavouritePage = () => {
             onRemoveAllFavourite={handleRemoveAllFavourite}></FavouriteToolbar>
 
           {/* Grid */}
-          <div className="min-h-70 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 md:gap-6 w-full mt-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 md:gap-6 w-full mt-8">
             {displayAnime.map((anime) => (
-              <div key={anime.mal_id} className="relative w-full">
+              <div
+                key={anime.mal_id}
+                className="relative w-full flex items-center justify-center ">
                 <AnimePosterCard className="w-full" anime={anime} />
               </div>
             ))}
@@ -90,7 +101,7 @@ const FavouritePage = () => {
               perPage={PER_PAGE}
               itemLength={totalItems}
               currentPage={currentPage}
-              isLoading={isLoading}
+              isLoading={false}
               onChangePage={() => {
                 // Already handle by in filteredAnime
               }}
